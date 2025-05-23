81bbaaa502438f40acc335304d6d3e7fe0b8f8c4
Oziel Sousa <ozielzin358@gmail.com>
2025-05-23 11:22:53 -0300

    refactor: Condicionais

--- src/instrucoes/EstruturasCondicionais.java
+++ src/instrucoes/EstruturasCondicionais.java
@@ -4,9 +4,9 @@ public class EstruturasCondicionais {
     public static void main (String [] args) {
         int num = 17;
         if(num >= 0){
-            System.out.println("O numero é Positivo");
+            System.out.println("O numero é Positivo!");
         } else {
-            System.out.println("O numero é negativo");
+            System.out.println("O numero é Negativo!");
         }
     }
 }
